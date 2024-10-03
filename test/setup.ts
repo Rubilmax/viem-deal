1b21895edbbb5e5730d2df1a8810c4ec4244c377
Rubilmax <rmilon@gmail.com>
2024-10-03 17:50:42 +0200

    test(config): add anvil timeout

--- test/setup.ts
+++ test/setup.ts
@@ -31,6 +31,7 @@ export const test = vitest.extend<{
     const instance = await anvil({
       forkUrl: rpcUrl,
       forkBlockNumber: 20_884_340,
+      timeout: 3_000,
     }).create({
       port: port++,
     });
